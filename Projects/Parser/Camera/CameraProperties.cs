638527adeb67b756f1934e62f0c799ebdf52d6cd
j1schmid <j1schmid@hsr.ch>
2013-02-24 19:35:12 +0100

    Camera implementiert. Es kann nun jeweils per GetLastFrame ein Bild von der Standard-Webcam geholt werden.

---
+++ Projects/Parser/Camera/CameraProperties.cs
@@ -0,0 +1,27 @@
+﻿namespace HSR.PresentationWriter.DataSources
+{
+    /// <summary>
+    /// Used to initialize an AVICAP-Webcam</summary>
+    public abstract class CameraProperties
+    {
+        /// <summary>
+        /// Width of the AVICAP virtual window</summary>
+        public int Width;
+
+        /// <summary>
+        /// Height of the AVICAP virtual window</summary>
+        public int Height;
+
+    }
+
+    /// <summary>
+    /// Used by Camera if nothing else is supplied.</summary>
+    public class StandardCameraProperties : CameraProperties
+    {
+        public StandardCameraProperties()
+        {
+            Width = 320;
+            Height = 240;
+        }
+    }
+}
---
+++ Projects/Parser/Camera/Frame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+
+namespace HSR.PresentationWriter.DataSources
+{
+    /// <summary>
+    /// Container for a captured webcam image</summary>
+    public class Frame
+    {
+        /// <summary>
+        /// Frame Index</summary>
+        public readonly int Number;
+
+        /// <summary>
+        /// Linked Picture</summary>
+        public readonly Image Image;
+
+        /// <summary>
+        /// Time when the picture was taken or when the frame was ready.
+        /// This depends on how you initialized the frame. If you didn't
+        /// Initialize the frame with a timestamp, the value is set to
+        /// the constructors ending time.</summary>
+        public readonly long Timestamp;
+
+        /// <summary>
+        /// Initializing a frame.
+        /// If no timestamp is given, we set one as soon the frame is ready</summary>
+        /// <param name="number">
+        /// Frame Index</param>
+        /// <param name="image">
+        /// Linked Picture</param>
+        /// <param name="timestamp">
+        /// Time when the picture was taken in milliseconds</param>
+        public Frame(int number, Image image, long timestamp = 0)
+        {
+            this.Number = number;
+            this.Image = image;
+            this.Timestamp = timestamp != 0 ? timestamp : CurrentMillis.Millis;
+        }
+    }
+}
