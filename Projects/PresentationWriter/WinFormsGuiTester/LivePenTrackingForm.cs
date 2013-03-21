e60b66ddf9e1bd06ff4b6caae21baabb99c00295
j1schmid <j1schmid@hsr.ch>
2013-03-21 15:22:45 +0100

    PointTracking Anzeige für die Filter Kalibrierung eingeführt

--- Projects/PresentationWriter/WinFormsGuiTester/LivePenTrackingForm.cs
+++ Projects/PresentationWriter/WinFormsGuiTester/LivePenTrackingForm.cs
@@ -5,12 +5,14 @@ using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinFormsGuiTester
 {
     public partial class LivePenTrackingForm : Form
     {
+        private FixedSizedQueue<PointFrame> penDrawingBuffer;
         private AForgeCamera camera;
         private AForgePenTracker tracker;
         private Graphics overlayGraphics;
@@ -18,6 +20,7 @@ namespace WinFormsGuiTester
 
         public LivePenTrackingForm()
         {
+            penDrawingBuffer = new FixedSizedQueue<PointFrame>(50);
             InitializeComponent();
         }
 
@@ -35,21 +38,45 @@ namespace WinFormsGuiTester
             camera.ShowConfigurationDialog();
         }
 
+        //private int lastRandX = 100;
+        //private int lastRandY = 100;
+        //Random tempRandom = new Random();
         private async void camera_FrameReady(object sender, FrameReadyEventArgs e)
         {
-            long time1 = CurrentMillis.Millis;
-            PointFrame p = await tracker.ProcessAsync(e.Frame);
-            long time2 = CurrentMillis.Millis;
-            Debug.WriteLine("Time: {0}", time2 - time1);
-            //PointFrame p = tracker.GetLastFrame();
-            this.calibrationPictureBox.Image = (Image)e.Frame.Bitmap.Clone();
+            Task<PointFrame> asyncPointFrame = tracker.ProcessAsync(e.Frame);
+            Bitmap redaction = (Bitmap)e.Frame.Bitmap.Clone();
+            PointFrame pointFrame = await asyncPointFrame;
+
+            //// fake:
+            //lastRandX += tempRandom.Next(15);
+            //lastRandY += tempRandom.Next(15);
+            //penDrawingBuffer.Enqueue(new PointFrame(1, new Point(lastRandX % 640, lastRandY % 480)));
+
+            // draw points in buffer to image
+            using (Graphics g = Graphics.FromImage(redaction))
+            {
+                using (SolidBrush brush = new SolidBrush(Color.Black))
+                {
+                    Point previousPoint = Point.Empty;
+                    foreach (PointFrame f in penDrawingBuffer)
+                    {
+                        g.DrawEllipse(Pens.Green, f.Point.X, f.Point.Y, 3, 3);
+                        if (!previousPoint.IsEmpty)
+                        {
+                            g.DrawLine(Pens.Red, previousPoint, f.Point);
+                        }
+                        previousPoint = f.Point;
+                    }
+                }
+                this.calibrationPictureBox.Image = redaction;
+            }
         }
 
 #if DEBUG
         private void tracker_DebugPicture(object sender, DebugPictureEventArgs e)
         {
             this.diffDebugPicture.Image = (Image)e.Pictures[0].Clone();
-            this.blobDebugPicture.Image = (Image)e.Pictures[1].Clone();
+            this.blobDebugPicture.Image = (Image)e.Pictures[2].Clone();
         }
 #endif
 
