eca97bd35e57d219e15e28e04cb8ee7397966bd5
j1schmid <j1schmid@hsr.ch>
2013-05-27 17:05:06 +0200

    Quad auch noch

---
+++ Projects/PresentationWriter/Parser/Quad.cs
@@ -0,0 +1,12 @@
+﻿using AForge;
+
+namespace HSR.PresWriter.PenTracking
+{
+    public struct Quad
+    {
+        public Point TopLeft { get; set; }
+        public Point TopRight { get; set; }
+        public Point BottomLeft { get; set; }
+        public Point BottomRight { get; set; }
+    }
+}
